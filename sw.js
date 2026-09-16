934b7a53bd56b2a023b1daa9dd6016b7f3b57fa7
BakaHideout <missgodthumbs@gmail.com>
2026-09-16 05:12:15 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'TotoQuestTestServerV2';
-const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const ASSETS = ['./index.html', './manifest.json', './icon-192.jpg', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
