9ce73579584448e4a09e68f39efa346a50151754
BakaHideout <missgodthumbs@gmail.com>
2026-09-16 05:34:02 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'TotoQuestTestServerV4';
+const CACHE_NAME = 'TotoQuestTestServerV5';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
